52b3c97949a0de18def92c45ee1b89198b95f7aa
Tuur Martens <tuurmartens4@gmail.com>
2021-11-05 22:39:47 +0100

    simplify accessing of parameters

--- src/main/kotlin/fm/rythm/commandhandler/textcommands/CommandContext.kt
+++ src/main/kotlin/fm/rythm/commandhandler/textcommands/CommandContext.kt
@@ -4,8 +4,5 @@ import net.dv8tion.jda.api.entities.Message
 
 data class CommandContext<Parameters>(
     val message: Message,
-    private val parameters: Parameters
-) : BaseContext(message) {
-    val getParameters: Parameters
-		get() { return parameters }
-}
+    val parameters: Parameters
+) : BaseContext(message)
